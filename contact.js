23e3cbd4e31531d2da233f3a92b4669b35d34a9c
alabi sodiq <adisco4420@gmail.com>
2019-09-04 09:53:30 +0100

    added contact form

---
+++ contact.js
@@ -0,0 +1,8 @@
+function myContact(){
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var title = document.getElementById('title');
+    var message = document.getElementById('message');
+    var submit = document.getElementById('submit');
+}
+
